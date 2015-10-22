01a9dd33049815ab6cd2f1f228947ef4d32d8345
Kiko Previero <kikoprevi@hotmail.com>
2015-10-22 16:16:23 -0200

    criada a classa metodos.

---
+++ socket/src/ambTeste/metodos.java
@@ -0,0 +1,9 @@
+package ambTeste;
+
+public class metodos {
+
+	public metodos(){
+		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("user.home"));
+	}
+}
--- socket/src/ambTeste/principal.java
+++ socket/src/ambTeste/principal.java
@@ -3,9 +3,7 @@ package ambTeste;
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(System.getProperty("user.dir"));
-		System.out.println(System.getProperty("user.home"));
+		new metodos();
 	}
 
 }

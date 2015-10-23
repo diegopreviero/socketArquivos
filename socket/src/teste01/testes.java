825782abf0474d735432fdb258b9462338936603
Kiko Previero <kikoprevi@hotmail.com>
2015-10-23 10:58:20 -0200

    commit.

---
+++ socket/src/teste01/testes.java
@@ -0,0 +1,23 @@
+package teste01;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class testes {
+
+	public static void main(String[] args) {
+
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				"JPG & GIF Images", "jpg", "gif");
+		chooser.setFileFilter(filter);
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		/*int returnVal = chooser.showOpenDialog("nome do frame");
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this file: " +
+					chooser.getSelectedFile().getName());
+		}*/
+
+	}
+
+}

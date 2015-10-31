f3b9ed4a96de15570feb5c05309ba1b715e96570
Kiko Previero <kikoprevi@hotmail.com>
2015-10-31 19:05:52 -0200

    criadas funcoes temporarias para teste de objetos.

---
+++ socket/src/anotacoes/Arquivo.java
@@ -0,0 +1,12 @@
+package anotacoes;
+
+public class Arquivo {
+
+	public Arquivo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	String raiz;
+	String nome;
+	int tamanho;
+}
---
+++ socket/src/anotacoes/funcao.java
@@ -0,0 +1,15 @@
+package anotacoes;
+
+public class funcao {
+
+	public funcao() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	int index;
+	String nome;
+	String parametro1;
+	String parametro2;
+	Arquivo arquivo;
+
+}

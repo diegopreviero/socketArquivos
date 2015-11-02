cd9ad19f1ce522448930cd1c0d97c731a554ce84
Kiko Previero <kikoprevi@hotmail.com>
2015-11-02 13:08:45 -0200

    alteracoes em InfoCliente.

--- socket/src/anotacoes/InfoCliente.java
+++ socket/src/anotacoes/InfoCliente.java
@@ -2,19 +2,57 @@ package anotacoes;
 
 public class InfoCliente {
 
-	public InfoCliente() {
-		// TODO Auto-generated constructor stub
+	public InfoCliente() { }
+	
+	String versaoCliente;
+	String versaoJava;
+	String statusDeep;
+	String os;
+	String escola; //setado no properties pelo servidor
+	String ip;
+	String hostName;
+	
+	public String getVersaoCliente() {
+		return versaoCliente;
 	}
-	
-	/*
-	 * versao cliente
-	 * versao java
-	 * status deep freeze
-	 * sistema operacional
-	 * escola (setado no properties pelo servidor)
-	 * 
-	 */
-	
-	
+	public void setVersaoCliente(String versaoCliente) {
+		this.versaoCliente = versaoCliente;
+	}
+	public String getVersaoJava() {
+		return versaoJava;
+	}
+	public void setVersaoJava(String versaoJava) {
+		this.versaoJava = versaoJava;
+	}
+	public String getStatusDeep() {
+		return statusDeep;
+	}
+	public void setStatusDeep(String statusDeep) {
+		this.statusDeep = statusDeep;
+	}
+	public String getOs() {
+		return os;
+	}
+	public void setOs(String os) {
+		this.os = os;
+	}
+	public String getEscola() {
+		return escola;
+	}
+	public void setEscola(String escola) {
+		this.escola = escola;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
 
 }
